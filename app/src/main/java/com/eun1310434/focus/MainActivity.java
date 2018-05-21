3b1a35f9914b78abdc572d624812ac16aad23797
eun1310434 <eun1310434@naver.com>
2018-05-21 17:54:31 +0200

    Initial commit

---
+++ app/src/main/java/com/eun1310434/focus/MainActivity.java
@@ -0,0 +1,26 @@
+/*=====================================================================
+□ Infomation
+  ○ Data : 21.05.2018
+  ○ Mail : eun1310434@naver.com
+  ○ Blog : https://blog.naver.com/eun1310434
+  ○ Reference : Do it android app Programming
+
+□ Function
+  ○
+
+□ Study
+  ○
+=====================================================================*/
+package com.eun1310434.focus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
